4f17796c1b2ce7136b2ab93a997ffa00e3a57feb
SntOp7 <santiago.ospinas@uqvirtual.edu.co>
2025-11-10 14:28:58 -0500

    feat: cambios en los nodos

--- Unidad3/nodos/nodo-principal.exs
+++ Unidad3/nodos/nodo-principal.exs
@@ -1,60 +1,54 @@
 defmodule NodoCliente do
-  @nombre_servicio_local :servicio_respuesta
-  @servicio_local {@nombre_servicio_local, :nodocliente@cliente}
-  @nodo_remoto :"nodoservidor@localhost"
-  @servicio_remoto {:servicio_cadenas, @nodo_remoto}
+
+  @nodo_cliente :"cliente@192.168.137.239"
+  @nodo_servidor :"servidor@192.168.137.1"
+  @nombre_proceso :servicio_cadenas
+
   # Lista de mensajes a procesar
   @mensajes [
     {:mayusculas, "Juan"},
     {:mayusculas, "Ana"},
     {:minusculas, "Diana"},
     {&String.reverse/1, "Julián"},
-    "Uniquindio",
-    :fin
+    "Uniquindio", :fin
   ]
 
   def main() do
-    Util.mostrar_mensaje("PROCESO PRINCIPAL")
-
-    @nombre_servicio_local
-    |> registrar_servicio()
-
-    establecer_conexion(@nodo_remoto)
+    IO.puts("SE INICIA EL CLIENTE")
+    iniciar_nodo(@nodo_cliente)
+    establecer_conexion(@nodo_servidor)
     |> iniciar_produccion()
   end
 
-  defp registrar_servicio(nombre_servicio_local),
-    do: Process.register(self(), nombre_servicio_local)
+  defp iniciar_nodo(nombre) do
+    Node.start(nombre)
+    Node.set_cookie(:my_cookie)
+  end
 
   defp establecer_conexion(nodo_remoto) do
     Node.connect(nodo_remoto)
   end
 
-
-  defp iniciar_produccion(false),
-    do: Util.mostrar_error("No se pudo conectar con el nodo servidor")
+  defp iniciar_produccion(false), do: IO.puts("No se pudo conectar con el nodo servidor")
 
   defp iniciar_produccion(true) do
     enviar_mensajes()
     recibir_respuestas()
   end
 
-
   defp enviar_mensajes() do
     Enum.each(@mensajes, &enviar_mensaje/1)
   end
 
   defp enviar_mensaje(mensaje) do
-    send(@servicio_remoto, {@servicio_local, mensaje})
+    send({@nombre_proceso, @nodo_servidor}, {self(), mensaje})
   end
 
   defp recibir_respuestas() do
     receive do
-      :fin ->
-        :ok
-
+      :fin -> :ok
       respuesta ->
-        Util.mostrar_mensaje("\t -> \"#{respuesta}\"")
+        IO.puts("\t -> \"#{respuesta}\"")
         recibir_respuestas()
     end
   end
